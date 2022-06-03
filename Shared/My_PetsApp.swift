cc0af5570a8536a68e1ac7120eb4a2e935c9d4c2
YazeedAlhanaki <tariq.almazyad@gmail.com>
2022-06-03 14:43:58 +0300

    Initial Commit

---
+++ Shared/My_PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PetsApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct My_PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
